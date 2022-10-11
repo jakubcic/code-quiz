4411e4be906116ce5017bc25c687333bed7cd6eb
jakub <jakub@uconn.edu>
2022-10-11 17:17:41 -0400

    create array to contain quiz questions, possible answers, and true answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,36 @@ var timer = document.querySelector("#timer");
 var questionSection = document.querySelector("#questionSection");
 var questionList = document.querySelector("#questionList");
 
+// questions array will contain an object for each question of the quiz
+// where the question, possible answers, and the true answer are held
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts"
+  },
+  {
+    question: "The condition in an if /else statement is enclosed within _____.",
+    options: ["1. quotes", "2. curly brackets", "3. parentheses", "4. square brackets"],
+    answer: "3. parentheses"
+  },
+  {
+    question: "Arrays in JavaScript can be used to store _____.",
+    options: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+    answer: "4. all of the above"
+  },
+  {
+    question: "String values must be enclosed within _____ when being assigned to variables.",
+    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+    answer: "3. quotes"
+  },
+  {
+    question: "A very useful tool used during development and debugging for printing content to the console is _____.",
+    options: ["1. JavaScript", "2. terminal", "3. print", "4. console.log"],
+    answer: "4. console.log"
+  }
+]
+
 // start a time interval when the startBtn is clicked
 // and display the quiz questions
 startBtn.addEventListener("click", function (){
