6327380c85741b5e67fabc58e68aabfc00896e92
jakub <jakub@uconn.edu>
2022-10-11 17:12:51 -0400

    add event listener for clicking start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,21 @@ var timer = document.querySelector("#timer");
 var questionSection = document.querySelector("#questionSection");
 var questionList = document.querySelector("#questionList");
 
+// start a time interval when the startBtn is clicked
+// and display the quiz questions
+startBtn.addEventListener("click", function (){
+  let timeInterval = 0;
+  let timeLeft = 75;
+  if (timeInterval === 0){
+    timeInterval = setInterval(function (){
+      timeLeft--;
+      timer.textContent = "Time: " + timeLeft;
+
+      if (timeLeft === 0) {
+        clearInterval(timeInterval);
+        timer.textContent = "You're out of time!";
+      }
+    }, 1000);
+  }
+  // call a function to display questions
+})
