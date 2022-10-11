89eac412400d66a9db7142b4e10578e1edfc9f5b
jakub <jakub@uconn.edu>
2022-10-11 17:10:11 -0400

    add some variables to select elements from html

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+// declare variables. need start button element, timer element,
+// main question section element, question list element
+var startBtn = document.querySelector("#startBtn");
+var timer = document.querySelector("#timer");
+var questionSection = document.querySelector("#questionSection");
+var questionList = document.querySelector("#questionList");
+
